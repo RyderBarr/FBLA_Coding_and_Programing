bd76f4f59cbf887aff889e50982f74b946170b5e
RyderBarr <tootwo2to22@gmail.com>
2024-09-10 17:35:39 -0700

    deleted class and fix methods
    deleted the transaction class, fixed the account methods.

--- acounts.py
+++ acounts.py
@@ -1,81 +1,9 @@
-
-# feeling cute might delete later :)
-# a class that is used to exicute and keep 
-# track of the acounts transactions 
-# including information like the amount, 
-# category, and date 
-#   |
-#   V
-class transaction:
-#transaction information
-
-        # Whether the transaction is a withdraw or deposite
-        # true is deposite V 
-    transaction_category = True
-
-        # the amount of money withdrew or deposited with a transaction V
-    transaction_amount = 0.00
-
-        # will save the date of a transaction V
-    transaction_date = ""
-
-        # the day that a transaction has happened 1-2 digit V
-    transaction_day = 0
-
-        # the day that a transaction has happened 1-2 digit V
-    transaction_month = 00
-
-        # the day that a transaction has happened 2 or 4 digit
-        # if its 2 digit we assume that it is the 2000's V
-    transaction_year = 0000
-
-#transaction information
-
-# transaction constroctor
-    def transaction(category, amount, day, month, year):
-        transaction_category = category
-        transaction_amount = amount
-        transaction_day = day
-        transaction_month = month
-        transaction_year = year
-# transaction constroctor
-
-#functions
-
-#converts an imputed date to a string
-    def date_to_string():
-        day = transaction_day
-        month = transaction_month
-        year = transaction_year
-            # these if-statments makes sure the the date is correct
-        #day check
-        if day > 31 or month < 0:
-            return "the day stated is incorrect"
-        #month check
-        if month > 12 or month < 0:
-            return "the month stated is incorrect"
-        #year check
-        if year > 2025 or year < 1900 and year > 99:
-            return "the year stated is incorrect"
-        if year < 100 and year >= 0:
-            year = 2000 + year
-
-            # converst the date to a string
-        date = str(month) + "/" + str(day) + "/" + str(year)
-
-            #assighns the day, month, and year to there class conterpars 
-        transaction_day = day
-        transaction_month = month
-        transaction_year = year
-        transaction_date = date
-
-            #returns a date in the form of a string
-        return date
-#converts an imputed date to a string
-
+# discription of class
     # a class that has all the acount information 
-    # such as acount name, balance, acount_income, acount_expense
-    #   |
+    # such as acount name, balance, acount_income, 
+    # acount_expense, acount_date and acount_dates
+    # as well as having many methods for 
+    # transactions and summaries
     #   V
 class acount_class:
 #acount information
@@ -95,6 +23,9 @@ class acount_class:
         # will save the date of an account creation 
     acount_date = ""
 
+        # will save pars of dates and transaction amounts
+    acount_dates = {}
+
  #acount information
 
 #acount_class constrocter
@@ -104,6 +35,7 @@ class acount_class:
         acount_income = income
         acount_expense = expense
         acount_date = date
+        acount_dates = dates
 #acount_class constrocter
     
 #functions V
@@ -138,7 +70,7 @@ class acount_class:
             acount_balance -= amount
 
             # adds this to the list of expenses
-            acount_expense.insert(amount)
+            acount_expense.append(amount)
             acount_expense.sort()
 
         elif category == True and amount > 2000:
@@ -149,148 +81,51 @@ class acount_class:
             acount_balance += amount
 
             # adds this to the list of incomes
-            acount_income.insert(amount)
+            acount_income.append(amount)
             acount_income.sort()
 
             # converst the date to a string
+            
         date = str(month) + "/" + str(day) + "/" + str(year)
-
+        dates.update({"date": amount})
             #returns a date in the form of a string
         return date
 # transaction 
 
 # uses the lists acount_expense and acount_income to form a summary
-    def daily_summaries():
+    def summaries():
         
         income = acount_income
         expense = acount_expense
 
             #income portion start
-        large_income = 1000
-       
-        num_large_income = income.count(large_income)
-        num_small_income = income.count() - income.count(large_income)
+        income.sort()
+        # need to use a for loop to see how many in a list are greater than a sertent num
+        large_in = [i for i in income if i>1000]
+        
+        num_large_income = len(large_in)
+        num_small_income = len(income) - len(large_in)
 
-        income_num = income.count()
+        income_num = len(income)
             #income portion end
 
             #expense portion start
-        large_expense = 1000
+        large_ex = [i for i in expense if i>1000]
        
-        num_large_expense = expense.count(large_expense)
-        num_small_expense = expense.count() - expense.count(large_expense)
+        num_large_expense = len(large_ex)
+        num_small_expense = len(expense) - len(large_ex)
 
-        expense_num = expense.count()
+        expense_num = len(expense)
             #expense portion end
 
-        summary = """ daily summary
-        there were """ + str(income_num) + incomes"""
-        there were """ + str(num_large_income) + """ incomes that exceded $1000 
-        there were  """  + str(num_small_income) +  """ incomes that are below $1000
-        there were """ + str(expense_num) + expenses"""
-        there were """ + str(num_large_expense) + """ expenses that exceded $1000 
-        there were  """  + str(num_small_expense) +  """ expenses that are below $1000
-         """ 
-        return summary
-    
-    
-    def weekly_summaries():
-
-        income = acount_income
-        expense = acount_expense
-
-            #income portion start
-        large_income = 1000
-       
-        num_large_income = income.count(large_income)
-        num_small_income = income.count() - income.count(large_income)
-
-        income_num = income.count()
-            #income portion end
-
-            #expense portion start
-        large_expense = 1000
-       
-        num_large_expense = expense.count(large_expense)
-        num_small_expense = expense.count() - expense.count(large_expense)
-
-        expense_num = expense.count()
-            #expense portion end
-
-        summary = """ weekly summary
-        there were """ + str(income_num) + incomes"""
-        there were """ + str(num_large_income) + """ incomes that exceded $1000 
-        there were  """  + str(num_small_income) +  """ incomes that are below $1000
-        there were """ + str(expense_num) + expenses"""
-        there were """ + str(num_large_expense) + """ expenses that exceded $1000 
-        there were  """  + str(num_small_expense) +  """ expenses that are below $1000
-         """ 
-        return summary
-    
-    
-    def monthly_summaries():
-
-        income = acount_income
-        expense = acount_expense
-
-            #income portion start
-        large_income = 1000
-       
-        num_large_income = income.count(large_income)
-        num_small_income = income.count() - income.count(large_income)
-
-        income_num = income.count()
-            #income portion end
-
-            #expense portion start
-        large_expense = 1000
-       
-        num_large_expense = expense.count(large_expense)
-        num_small_expense = expense.count() - expense.count(large_expense)
-
-        expense_num = expense.count()
-            #expense portion end
-
-        summary = """ monthly summary
-        there were """ + str(income_num) + incomes"""
-        there were """ + str(num_large_income) + """ incomes that exceded $1000 
-        there were  """  + str(num_small_income) +  """ incomes that are below $1000
-        there were """ + str(expense_num) + expenses"""
-        there were """ + str(num_large_expense) + """ expenses that exceded $1000 
-        there were  """  + str(num_small_expense) +  """ expenses that are below $1000
-         """ 
-        return summary
-    
-    def yearly_summaries():
-
-        income = acount_income
-        expense = acount_expense
-
-            #income portion start
-        large_income = 1000
-
-        num_large_income = income.count(large_income)
-        num_small_income = income.count() - income.count(large_income)
-
-        income_num = income.count()
-            #income portion end
-
-            #expense portion start
-        large_expense = 1000
-       
-        num_large_expense = expense.count(large_expense)
-        num_small_expense = expense.count() - expense.count(large_expense)
-
-        expense_num = expense.count()
-            #expense portion end
-
-        summary = """ yearly summary
-        there were """ + str(income_num) + incomes"""
-        there were """ + str(num_large_income) + """ incomes that exceded $1000 
-        there were  """  + str(num_small_income) +  """ incomes that are below $1000
-        there were """ + str(expense_num) + expenses"""
-        there were """ + str(num_large_expense) + """ expenses that exceded $1000 
-        there were  """  + str(num_small_expense) +  """ expenses that are below $1000
-         """ 
+        summary = '''
+        summary
+        there were ''' + str(income_num) + ''' incomes
+        there were ''' + str(num_large_income) + ''' incomes that exceded $1000 
+        there were  '''  + str(num_small_income) +  ''' incomes that are below $1000
+        there were ''' + str(expense_num) + ''' expenses
+        there were ''' + str(num_large_expense) + ''' expenses that exceded $1000 
+        there were  '''  + str(num_small_expense) +  ''' expenses that are below $1000
+         ''' 
         return summary
 # uses the lists acount_expense and acount_income to form a summary
