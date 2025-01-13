4a8c598baea2206668eeeb01890ec8135a7cec71
ryder <tootwo2to22@gmail.com>
2025-01-12 19:32:11 -0700

    1/12/25 - transfer computers

--- failes/User.js
+++ failes/User.js
@@ -40,30 +40,44 @@ module.exports = { read, write }
 
 let username = "J0hnDo3"
 let password = "123321"
-let phone = "123-456-7890"
 let email = "johndoe@gmail.com"
+let balance = 0
+let fName = "John"
+let lName = "Doe"
 
 let data = fs.readFileSync("UserDataBase.json", 'utf8')
 let users = JSON.parse(data);
-
-// if phone number is writen like ***-***-****
-    phonecheck:if ( phone.length == 12 )
-    { 
-
-        phone = phone.substring(0,3) + phone.substring(4,7) + phone.substring(8); //cuting out the - or / in the number
-        this.phone = phone;
-        
-    }
-
-//if phone is writen like **********
-    phonecheck:if ( phone.length == 10 )
-    {
-
-       this.phone = phone;
-
-    }
-
 let lastJson = 0
+
+
+function collect()
+{
+    username = document.getElementById("username")
+    password = document.getElementById("password")
+    email = document.getElementById("email")
+    balance = document.getElementById("balance")
+    fName = document.getElementById("fname")
+    lName = document.getElementById("lname")
+    
+
+    for (i in users){
+        lastJson = i - 1 + 2
+    }
+
+    const Obj = {
+        userName: username, 
+        userPassword: password, 
+        userEmail: email,
+        userBalance: balance,
+        userFName: fName,
+        userLName: lName,
+        id: [lastJson]
+    
+    }
+    users.push(Obj)
+    
+    write(users)
+}
 
 for (i in users){
     lastJson = i - 1 + 2
@@ -72,8 +86,10 @@ for (i in users){
 const Obj = {
     userName: username, 
     userPassword: password, 
-    userPhone: phone,
     userEmail: email,
+    userBalance: balance,
+    userFName: fName,
+    userLName: lName,
     id: [lastJson]
 
 }
---
+++ user-data/code/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const morgan = require('morgan')
+const {prohairesis} = require('prohairesis')
+// const bodyparser = require('body-parser')
+
+
+
+const app = express()
+const port = process.env.PORT || 8080
+
+app
+    // gives any static file in the public folder
+    .use(express.static('public'))
+    // logs everything in development mode
+    .use(morgan('dev'))
+    // parse application/x-www-form-urlencoded
+    .use(express.urlencoded({ extend: false}))
+    //parse application/json
+    .use(express.json())
+
+    // port varible from abov e, a nameless fuction that writes out what port the server is listening to
+    .listen(port, () => console.log(`server on ${port}`))
